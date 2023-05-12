86e26d34ef8f6e25fa4a5cb66da229d2d671531e
yuvasaiganesh <130994702+yuvasaiganesh@users.noreply.github.com>
2023-05-12 19:54:26 +0530

    Update index.js

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,9 +1,54 @@
 // Write your code here
-const TransactionItem = props => {
-  const {transactionTypeDetails} = props
-  const {optionId, displayText} = transactionTypeDetails
+import './index.css'
 
-  return <option value={optionId}>{displayText}</option>
+const TransactionItem = props => {
+  const {balance, income, expenses} = props
+
+  return (
+    <>
+      <div className="balanceSection">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          alt="balance"
+          className="transactionImg"
+        />
+        <div className="headingSection">
+          <p className="heading2">Your Balance</p>
+          <p data-testid="balanceAmount" className="paragraph2">
+            RS {balance}
+          </p>
+        </div>
+      </div>
+
+      <div className="balanceSection">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png "
+          alt="income"
+          className="transactionImg"
+        />
+        <div className="headingSection">
+          <p className="heading2">Your Income</p>
+          <p data-testid="incomeAmount" className="paragraph2">
+            RS {income}{' '}
+          </p>
+        </div>
+      </div>
+
+      <div className="balanceSection">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png "
+          alt="expenses"
+          className="transactionImg"
+        />
+        <div className="headingSection">
+          <p className="heading2">Your Expenses</p>
+          <p data-testid="expensesAmount" className="paragraph2">
+            Rs {expenses}
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default TransactionItem
